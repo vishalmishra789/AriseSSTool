4fbbcdae4bc4e58989db7f370e5eeb985e452d2f
Vishal Mishra <vishalmishra739831@gmail.com>
2026-04-12 21:12:59 +0530

    Create Launcher.java

---
+++ src/main/java/com/arise/ss/Launcher.java
@@ -0,0 +1,8 @@
+package com.arise.ss;
+
+public class Launcher {
+    public static void main(String[] args) {
+        // This bypasses the JavaFX runtime check and allows the EXE to start
+        MainApp.main(args);
+    }
+}
