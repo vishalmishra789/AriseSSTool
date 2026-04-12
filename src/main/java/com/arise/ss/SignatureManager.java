772499f4671750fd9fae243f3234825efa8a5823
Vishal Mishra <vishalmishra739831@gmail.com>
2026-04-12 22:34:07 +0530

    Update SignatureManager.java

--- src/main/java/com/arise/ss/SignatureManager.java
+++ src/main/java/com/arise/ss/SignatureManager.java
@@ -1,1 +1,26 @@
+package com.arise.ss;
 
+import com.google.gson.Gson;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SignatureManager {
+    public List<CheatHash> blacklistedHashes = new ArrayList<>();
+    public List<String> blacklistedProcesses = new ArrayList<>();
+    public List<String> suspiciousStrings = new ArrayList<>();
+
+    public static SignatureManager load() {
+        try (FileReader reader = new FileReader("signatures.json")) {
+            return new Gson().fromJson(reader, SignatureManager.class);
+        } catch (Exception e) {
+            System.out.println("No signature database found, using heuristics only.");
+            return new SignatureManager();
+        }
+    }
+
+    public static class CheatHash {
+        public String name;
+        public String hash;
+    }
+}
